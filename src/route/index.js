fc4e0789271b3e9500a8a0e5b12328d9eee97d54
Olga-Gagarina <129504976+Olga-Gagarina@users.noreply.github.com>
2023-08-01 14:39:46 +0300

    slack

--- src/route/index.js
+++ src/route/index.js
@@ -1103,4 +1103,13 @@ router.get('/template-7', function (req, res) {
   })
   //                  ↑↑ сюди вводимо JSON дані
 })
+router.get('/slack', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  //            ↙ cюди вводимо назву файлу з сontainer
+  res.render('slack', {
+    layout: null,
+  })
+  //                  ↑↑ сюди вводимо JSON дані
+})
 module.exports = router
